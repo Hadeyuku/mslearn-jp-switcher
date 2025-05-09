690ab2453d094a15df0faa77a9e6b390f8505ec5
“Hideyuki <nakagawahide35@gmail.com>
2025-05-09 20:03:26 +0900

    Remove dist and zip from repository; update .gitignore

--- dist/background.js
+++
@@ -1,6 +0,0 @@
-chrome.action.onClicked.addListener(async (tab) => {
-    if (!tab.url) return;
-    const jp = tab.url.replace(/\/en-us\//i, "/ja-jp/");
-    if (jp !== tab.url) await chrome.tabs.update(tab.id, { url: jp });
-  });
-  
