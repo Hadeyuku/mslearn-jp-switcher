b1004d4a9826f06aa5471a6a7910c128757f2467
“Hideyuki <nakagawahide35@gmail.com>
2025-05-11 10:45:25 +0900

    add jp to en

--- src/background.js
+++ src/background.js
@@ -1,6 +1,18 @@
 chrome.action.onClicked.addListener(async (tab) => {
-    if (!tab.url) return;
-    const jp = tab.url.replace(/\/en-us\//i, "/ja-jp/");
-    if (jp !== tab.url) await chrome.tabs.update(tab.id, { url: jp });
-  });
-  
+  if (!tab.url) return;
+
+  let newUrl = tab.url;
+
+  // 英語 → 日本語
+  if (/\/en-us\//i.test(tab.url)) {
+    newUrl = tab.url.replace(/\/en-us\//i, "/ja-jp/");
+  }
+  // 日本語 → 英語
+  else if (/\/ja-jp\//i.test(tab.url)) {
+    newUrl = tab.url.replace(/\/ja-jp\//i, "/en-us/");
+  }
+
+  if (newUrl !== tab.url) {
+    await chrome.tabs.update(tab.id, { url: newUrl });
+  }
+});
